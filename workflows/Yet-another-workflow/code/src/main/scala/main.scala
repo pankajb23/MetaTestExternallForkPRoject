6c193086c33b2fada042bff17b1217e1ebdc901c
pankaj <pankaj@simpledatalabs.com>
2021-08-20 19:01:17 +0000

    New Commit for Workflow Code

---
+++ workflows/Yet-another-workflow/code/src/main/scala/main.scala
@@ -0,0 +1,42 @@
+import io.prophecy.libs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import config.ConfigStore._
+import udfs.UDFs._
+import udfs._
+
+import graph._
+
+@Visual(mode = "batch", interimMode = "full")
+object Main {
+
+  def graph(spark: SparkSession): Unit = {
+
+    val df_Source0: Source = Source0(spark)
+    Target0(spark, df_Source0)
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    import config._
+    ConfigStore.Config = ConfigurationFactoryImpl.fromCLI(args)
+
+    val spark = SparkSession
+      .builder()
+      .appName("Yetanotherworkflow")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    UDFs.registerUDFs(spark)
+    UDAFs.registerUDAFs(spark)
+
+    val sc = spark.sparkContext
+    sc.setCheckpointDir("/tmp/checkpoints")
+
+    graph(spark)
+  }
+
+}
